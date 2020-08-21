5aadb851f2b569d3e84692d27b9cef7cb490afc7
taskoff <marian_taskov@abv.bg>
2020-08-21 12:56:21 +0300

    change db.url

--- restapi/config/config.js
+++ restapi/config/config.js
@@ -3,11 +3,11 @@ module.exports = {
     development: {
         port: process.env.PORT || 4000,
         privateKey: 'MY_VERY_SECRET_KEY',
-        databaseUrl: 'mongodb+srv://marcho:marcho-password@cluster0.tfnjr.mongodb.net/pets?retryWrites=true&w=majority'
+        databaseUrl: process.env.MONGODB_URL
     },
     production: {
         port: process.env.PORT || 4000,
         privateKey: 'MY_VERY_SECRET_KEY',
-        databaseUrl: 'mongodb+srv://marcho:marcho-password@cluster0.tfnjr.mongodb.net/pets?retryWrites=true&w=majority'
+        databaseUrl: process.env.MONGODB_URL
     }
 };
